652c972f4e37aa8808c850061480fe425b5441ba
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-15 15:38:31 -0400

    finsihed inserting swagger annotations for course controller endpoints

--- school/src/main/java/com/lambdaschool/school/config/Swagger2Config.java
+++ school/src/main/java/com/lambdaschool/school/config/Swagger2Config.java
@@ -7,7 +7,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,10 +21,10 @@ public class Swagger2Config
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors
-						.basePackage("com.lambdaschool.restaurants"))
+						.basePackage("com.lambdaschool.school"))
 				.paths(PathSelectors.any())
-				.build()
 				//                .paths(PathSelectors.regex("/**"))
+				.build()
 				.useDefaultResponseMessages(false) // Allows only my exception responses
 				.ignoredParameterTypes(Pageable.class) // allows only my paging parameter list
 				.apiInfo(apiEndPointsInfo());
@@ -35,7 +34,6 @@ public class Swagger2Config
 	{
 		return new ApiInfoBuilder().title("Restaurant Example")
 				.description("Restaurant Example")
-				.license("MIT").licenseUrl("https://github.com/LambdaSchool/java-crudysnacks/blob/master/LICENSE")
 				.version("1.0.0").build();
 	}
 }
--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -2,7 +2,10 @@ package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Course;
 import com.lambdaschool.school.service.CourseService;
-import com.lambdaschool.school.view.CountStudentsInCourses;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,9 @@ public class CourseController
     @Autowired
     private CourseService courseService;
 
+    @ApiOperation(value ="Returns all Courses", response = Course.class, responseContainer = "List")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "List of Courses Found", response = Course.class, responseContainer = "List"),
+                            @ApiResponse(code = 404, message = "List of Courses Not Found")})
     @GetMapping(value = "/courses", produces = {"application/json"})
     public ResponseEntity<?> listAllCourses()
     {
@@ -24,14 +30,20 @@ public class CourseController
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
     }
 
+    @ApiOperation(value ="Returns Count of Students in Each Course", responseContainer = "List")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "List Found",  responseContainer = "List"),
+            @ApiResponse(code = 404, message = "List Not Found")})
     @GetMapping(value = "/studcount", produces = {"application/json"})
     public ResponseEntity<?> getCountStudentsInCourses()
     {
         return new ResponseEntity<>(courseService.getCountStudentsInCourse(), HttpStatus.OK);
     }
 
+    @ApiOperation(value ="Deletes Course Depending on CourseId")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Course Deleted"),
+            @ApiResponse(code = 404, message = "Course Not Deleted")})
     @DeleteMapping("/courses/{courseid}")
-    public ResponseEntity<?> deleteCourseById(@PathVariable long courseid)
+    public ResponseEntity<?> deleteCourseById(@ApiParam(value = "Course Id", required = true, example = "1") @PathVariable long courseid)
     {
         courseService.delete(courseid);
         return new ResponseEntity<>(HttpStatus.OK);
