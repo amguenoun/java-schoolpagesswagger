404820a1553a07eb7bac13875be6d3fba533edde
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-15 16:24:34 -0400

    added paging endpoint for courses controller with swagger doc

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -2,11 +2,10 @@ package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Course;
 import com.lambdaschool.school.service.CourseService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +27,23 @@ public class CourseController
     {
         ArrayList<Course> myCourses = courseService.findAll();
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
+    }
+
+    @ApiOperation(value ="Returns all Courses - With Paging Support", response = Course.class, responseContainer = "List")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "List of Courses Found", response = Course.class, responseContainer = "List"),
+            @ApiResponse(code = 404, message = "List of Courses Not Found")})
+    @ApiImplicitParams({ //gives swagger info on the implicit query params
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
+    @GetMapping(value = "/courses/paging", produces = {"application/json"})
+    public ResponseEntity<?> listAllCoursesByPage(@PageableDefault(page=0, size=3) Pageable pageable){
+        return new ResponseEntity<>(courseService.findAllPageable(pageable), HttpStatus.OK);
     }
 
     @ApiOperation(value ="Returns Count of Students in Each Course", responseContainer = "List")
